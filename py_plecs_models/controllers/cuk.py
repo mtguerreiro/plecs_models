a7a9f27e710214f75f1d017e4cfc67daac88e762
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2026-02-01 17:53:16 +0100

    Now adding python interface

--- py_plecs_models/controllers/cuk.py
+++ py_plecs_models/controllers/cuk.py
@@ -3,6 +3,43 @@ import scipy.signal
 import pyctl
 import py_plecs_models as ppm
 
+
+def casc_fblin_params(design_params, plant_params, prefix=''):
+
+    t_settling = design_params['t_settling']
+    os = design_params['os']
+    if 'alpha' in design_params:
+        alpha = design_params['alpha']
+    else:
+        alpha = 10
+
+    model = design_params['model']
+    ts = design_params['ts']
+
+    if model == 'continuous':
+        model = 1
+    elif model == 'discrete':
+        model = 2
+    else:
+        raise TypeError('`model` should be `continuous` or `discrete`.')
+
+    Li = plant_params['Li']
+    Co = plant_params['Co']
+
+    k_ei, kv, k_ev = pyctl.design.pe.cuk.casc_fblin(
+        t_settling, os, L, C, alpha=alpha
+        )
+
+    _params = {
+        'k_ei': k_ei, 'k_ev': k_ev, 'kv': kv,
+        'model': model, 'ts': ts
+    }
+
+    params = ppm.ppm_utils.add_key_prefix_dict(_params, prefix)
+    
+    return params
+
+
 def energy_params(design_params, plant_params, prefix=''):
 
     t_settling = design_params['t_settling']
